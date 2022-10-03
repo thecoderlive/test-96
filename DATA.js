82f9d5039ba17734ce014ce0556317f19fb49c56
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-03 14:09:25 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1b5c0fcf-e424-47ad-89d9-4f9f0e2e67ba",
+    "home_feed": [
+        {
+            "id": "93fed3ea-3b77-4f98-8744-ab1cc5cdfdce",
+            "artist_name": "few",
+            "home_carousel": [
+                {
+                    "id": "4371b1f2-7510-42dc-858e-ba308aef9ecf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+                },
+                {
+                    "id": "ef33117a-7885-463f-bcdf-e6a997f05bf5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                },
+                {
+                    "id": "a2522d88-c5d7-4174-956f-31d37d7c910a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/82.png"
+                },
+                {
+                    "id": "14139ce1-b592-4358-b9d2-5159c6551640",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "31931119-8462-4fd2-8e51-1776714961b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                }
+            ],
+            "art_caption": "family",
+            "art_description": "Hour poor call necessary best. Whether probably believe performance coach community. Several contain international. Another talk last dark room health."
+        },
+        {
+            "id": "c8b7254e-bdbb-4a97-9a78-e0cedbb1240c",
+            "artist_name": "dog",
+            "home_carousel": [
+                {
+                    "id": "e19a5638-05de-4bbb-b584-f6b04a8fbf87",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "875375eb-f26e-4f74-a852-1f4de23fb0f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png"
+                },
+                {
+                    "id": "7a3dea97-c40c-4ea8-8e3d-7c9d7a036592",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "ed4facc2-c285-46ca-bfe3-f5a104ef4d67",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+                },
+                {
+                    "id": "fb57b9df-3e7d-418d-b788-b6b9683ec9f7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+                }
+            ],
+            "art_caption": "visit",
+            "art_description": "Call fast beyond yes successful. Hand leader always show door."
+        },
+        {
+            "id": "ae9460da-fa92-4113-951d-51c61d8c7804",
+            "artist_name": "human",
+            "home_carousel": [
+                {
+                    "id": "4f1f7ec4-afcc-496b-a13b-4967695f8249",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/69.png"
+                },
+                {
+                    "id": "57efab17-be09-44cb-bdbe-ff5baf66d740",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "bd2f1d1f-f30e-4395-89c1-745f2387751c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "6b4be47a-3fa9-4754-92af-70acd4f9fef2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png"
+                },
+                {
+                    "id": "b8e42800-8e25-47aa-a4b5-48ba25e6f856",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                }
+            ],
+            "art_caption": "boy",
+            "art_description": "Mean visit stay leg job fish agent center. Interview serve instead relationship. Although five fast south expect cultural religious."
+        },
+        {
+            "id": "77fe857e-80c7-463f-b7ab-9d26558af63a",
+            "artist_name": "skill",
+            "home_carousel": [
+                {
+                    "id": "8d9eb90b-978c-4167-83b9-0bbf138336c8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+                },
+                {
+                    "id": "be19c7f4-6948-4618-9440-b15ec746297b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "03d91281-54bc-4e7d-8f76-f72f2de097e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                },
+                {
+                    "id": "8946c4a5-077d-48af-972b-777c71e9afb2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png"
+                },
+                {
+                    "id": "37e57ea4-95a9-4bca-8b56-309264b9f572",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                }
+            ],
+            "art_caption": "oil",
+            "art_description": "Sea station buy. Foot father space employee must coach morning."
+        },
+        {
+            "id": "2cf0ad15-1300-4ffc-9b07-fb4f9968d192",
+            "artist_name": "weight",
+            "home_carousel": [
+                {
+                    "id": "4ee246d5-d08f-4349-9490-11de97145745",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/283.png"
+                },
+                {
+                    "id": "8da7708e-31d9-4bbd-a074-ba93168bb2bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/284.png"
+                },
+                {
+                    "id": "287e10c3-cd34-4907-bf9a-5094661ddb39",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                },
+                {
+                    "id": "1c47b8c5-6a6f-413d-a088-cd99483c4e8b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/219.png"
+                },
+                {
+                    "id": "3804c396-4eac-4763-a98a-fcaf2c24e5fb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/146.png"
+                }
+            ],
+            "art_caption": "world",
+            "art_description": "Member red national third relate other development. Country teacher that road anyone. Strategy describe theory happy explain floor."
+        }
+    ]
+}
